95c97940c28af658a7aa10a48005e3d7565ef7cc
Sabrina <sabrinacunsolo85@gmail.com>
2020-10-12 14:14:50 +0200

    Password generator

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+var nome_utente = prompt('Ciao, come ti chiami?');
+
+var cognome_utente = prompt('Qual è il tuo cognome?');
+
+var colore_preferito = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('password').innerHTML = nome_utente + cognome_utente + colore_preferito + '20';
